62f9b3983b24b2d51659dbf665a813677036f268
LinuxSourced <50146497+LinuxSourced@users.noreply.github.com>
2020-04-20 12:41:25 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,11 @@
 var irc = require('irc');
-var client = new irc.Client('irc.yourserver.com', 'myNick', {
-    channels: ['#channel'],
+var client = new irc.Client("irc.yourserver.com", "Chat-Utils", {
+    channels: ['#channel-name'],
+});
+
+channel.createWebhook('Chat-Utils');
+webhook.edit({
+	name: 'Chat-Utils',
+	avatar: '',
+	channel: '',
 });
